e76121faba1c1c3d71a87b71e323bb3bc814ff2d
Kerry Beth Richardson <kerrybethrichardson@Kerrys-MacBook-Pro-2.local>
2021-11-04 17:39:22 -0500

    new entry route bug fix

--- src/components/NewEntry.js
+++ src/components/NewEntry.js
@@ -3,15 +3,15 @@ import React from 'react'
 const NewEntry = () => {
 
     return (
-        <form onSubmit={handleNewEntrySubmit}>
+        <form onSubmit={props.handleNewEntrySubmit()}>
             <label for="date">Date:</label>
-            <input type="date" name="date" onChange={handleNewDateChange}/><br/>
+            <input type="date" name="date" onChange={props.handleNewDateChange()}/><br/>
             <label for="title">Title:</label>
-            <input type="text" name="title" onChange={handleNewTitleChange}/><br/>
+            <input type="text" name="title" onChange={props.handleNewTitleChange()}/><br/>
             <label for="entry">Entry:</label>
-            <textarea name="entry" onChange={handleNewEntryChange}/><br/>
+            <textarea name="entry" onChange={props.handleNewEntryChange()}/><br/>
             <label for="share">Public:</label>
-            <input type="checkbox" name="share" onChange={handleNewShareChange}/><br/>
+            <input type="checkbox" name="share" onChange={props.handleNewShareChange()}/><br/>
             <input type="submit" value="Submit Journal Entry"/>
         </form>
     )
