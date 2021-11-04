acee8dd2f56df3a08c0b343ec9220af8c54ca066
Kerry Beth Richardson <kerrybethrichardson@Kerrys-MacBook-Pro-2.local>
2021-11-04 10:42:50 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    // nav bar to include: link to homepage, log in, sign up, create new entry
+    // divs in sections will be user's card with name and photo
+    // footer will stay at bottom of page and have links to our githubs, etc
+
+    // const showLogIn = () => {
+    //     return (
+    //         <form onSubmit={handleLogIn}>
+    //             <label for="username">User name:</label>
+    //             <input type="text" name="username" required/>
+    //             <label for="password"> Password:</label>
+    //             <input type="password" name="password" required/>
+    //             <input type="submit" value="Log In"/>
+    //         </form>
+    //     )
+    // }
+    // const handleLogIn = (event) => {
+    //     event.preventDefault();
+    //     ///// create new sessions here /////
+    //     ////// authentication goes here /////
+    // }
+    // const showSignUp = () => {
+    //     return (
+    //         <form onSubmit={handleSignUp}>
+    //             <label for="username">User name:</label>
+    //             <input type="text" name="username" required/>
+    //             <label for="password"> Password:</label>
+    //             <input type="password" name="password" required/>
+    //             <input type="submit" value="Sign Up"/>
+    //         </form>
+    //     )
+    // }
+    // const handleSignUp = () => {
+    //     event.preventDefault();
+    //     //// create new user here /////
+    // }
+    // const showNewEntryBox = () => {
+    //     return (
+    //         <form onSubmit={()=>{createNewEntry}}>
+    //             <label for="entry">Your entry:</label>
+    //             <input type="textarea" name="entry" required/>
+    //             <input type="submit" value="Submit"/>
+    //         </form>
+    //     )
+    // }
+    return (
+        <h1>Hello world</h1>
+    )
+    // return (
+    //     <main>
+    //         <h1>KBJ Journal App</h1>
+    //         <nav>
+    //             <li onClick={showLogIn}>Log In</li>
+    //             <li onClick={showSignUp}>Sign Up</li>
+    //             <li onClick={showNewEntryBox}>New Entry</li>
+    //         </nav>
+    //         <section>
+    //             {
+    //                 users.map((user) => {
+    //                     return (<div>
+    //                             <h3>user.username</h3>
+    //                             <img src={user.photo} alt="profile photo of user"/>
+    //                             <button>View Profile</button>
+    //                         </div>
+    //                     )
+    //                 })
+    //             }
+    //         </section>
+    //         <footer></footer>
+    //     </main>
+    // )
 }
+
 
 export default App;
