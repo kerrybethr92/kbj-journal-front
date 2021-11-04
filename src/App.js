8e7c741314979520d49ccec7d3d345e3814629b2
Kerry Beth Richardson <kerrybethrichardson@Kerrys-MacBook-Pro-2.local>
2021-11-04 12:13:08 -0500

    testing index of entries

--- src/App.js
+++ src/App.js
@@ -1,86 +1,115 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
 
 const App = () => {
+    const [entries, setEntries] = useState([])
+
+    useEffect(() => {
+        axios
+            .get('https://journal-back-kbj.herokuapp.com/entries')
+            .then((response) => {
+                setEntries(respones.data)
+            })
+    })
+
+    return (
+        <main>
+            <section>
+                <h2>Journal entries:</h2>
+                <ul>
+                    {
+                        entries.map((entry) => {
+                            return <li>
+                            {entry.data}<br/>
+                            {entry.title}<br/>
+                            {entry.log}<br/>
+                            </li>
+                        })
+                    }
+                </ul>
+            </section>
+        </main>
+    )
     // nav bar to include: link to homepage, log in, sign up, create new entry
     // divs in sections will be user's card with name and photo
     // footer will stay at bottom of page and have links to our githubs, etc
 
-    const showLogIn = () => {
-        return (
-            <form onSubmit={handleLogIn}>
-                <label for="username">User name:</label>
-                <input type="text" name="username" required/>
-                <label for="password"> Password:</label>
-                <input type="password" name="password" required/>
-                <input type="submit" value="Log In"/>
-            </form>
-        )
-    }
-    const handleLogIn = (event) => {
-        event.preventDefault();
-        ///// create new sessions here /////
-        ////// authentication goes here /////
-    }
-    const showSignUp = () => {
-        return (
-            <form onSubmit={handleSignUp}>
-                <label for="username">User name:</label>
-                <input type="text" name="username" required/>
-                <label for="password"> Password:</label>
-                <input type="password" name="password" required/>
-                <input type="submit" value="Sign Up"/>
-            </form>
-        )
-    }
-    const handleSignUp = () => {
-        event.preventDefault();
-        //// create new user here /////
-    }
-    const showNewEntryBox = () => {
-        let entryForm = document.getElementById('new-entry');
-        if (entryForm.style.display === "none") {
-            entryForm.style.display = "block"
-        } else {
-            entryForm.style.display = "none";
-        }
-    }
-    return (
-        <main>
-            <h1>KBJ Journal App</h1>
-            <nav>
-                <li onClick={showLogIn}>Log In</li>
-                <li onClick={showSignUp}>Sign Up</li>
-                <li onClick={showNewEntryBox}>New Entry</li>
-            </nav>
-            <section>
-                <form id="new-entry" onSubmit={()=>{createNewEntry}}>
-                    <label for="date">Date:</label>
-                    <input type="date" name="date"/>
-                    <label for="title">Title:</label>
-                    <input type="text" name="title"/>
-                    <label for="log">Your entry:</label>
-                    <input type="textarea" name="log" required/>
-                    <label for="share">Public:</label>
-                    <input type="checkbox" name="share"/>
-                    <input type="submit" value="Submit"/>
-                </form>
-            </section>
-            <section>
-                {
-                    users.map((user) => {
-                        return (<div>
-                                <h3>user.username</h3>
-                                <img src={user.photo} alt="profile photo of user"/>
-                                <button>View Profile</button>
-                            </div>
-                        )
-                    })
-                }
-            </section>
-            <footer></footer>
-        </main>
-    )
+    // const showLogIn = () => {
+    //     return (
+    //         <form onSubmit={handleLogIn}>
+    //             <label for="username">User name:</label>
+    //             <input type="text" name="username" required/>
+    //             <label for="password"> Password:</label>
+    //             <input type="password" name="password" required/>
+    //             <input type="submit" value="Log In"/>
+    //         </form>
+    //     )
+    // }
+    // const handleLogIn = (event) => {
+    //     event.preventDefault();
+    //     ///// create new sessions here /////
+    //     ////// authentication goes here /////
+    // }
+    // const showSignUp = () => {
+    //     return (
+    //         <form onSubmit={handleSignUp}>
+    //             <label for="username">User name:</label>
+    //             <input type="text" name="username" required/>
+    //             <label for="password"> Password:</label>
+    //             <input type="password" name="password" required/>
+    //             <input type="submit" value="Sign Up"/>
+    //         </form>
+    //     )
+    // }
+    // const handleSignUp = () => {
+    //     event.preventDefault();
+    //     //// create new user here /////
+    // }
+    // const showNewEntryBox = () => {
+    //     let entryForm = document.getElementById('new-entry');
+    //     if (entryForm.style.display === "none") {
+    //         entryForm.style.display = "block"
+    //     } else {
+    //         entryForm.style.display = "none";
+    //     }
+    // }
+    // return (
+    //     <main>
+    //         <h1>KBJ Journal App</h1>
+    //         <nav>
+    //             <li onClick={showLogIn}>Log In</li>
+    //             <li onClick={showSignUp}>Sign Up</li>
+    //             <li onClick={showNewEntryBox}>New Entry</li>
+    //         </nav>
+    //         <section>
+    //             <form id="new-entry" onSubmit={()=>{createNewEntry}}>
+    //                 <label for="date">Date:</label>
+    //                 <input type="date" name="date"/>
+    //                 <label for="title">Title:</label>
+    //                 <input type="text" name="title"/>
+    //                 <label for="log">Your entry:</label>
+    //                 <input type="textarea" name="log" required/>
+    //                 <label for="share">Public:</label>
+    //                 <input type="checkbox" name="share"/>
+    //                 <input type="submit" value="Submit"/>
+    //             </form>
+    //         </section>
+    //         <section>
+    //             {
+    //                 users.map((user) => {
+    //                     return (<div>
+    //                             <h3>user.username</h3>
+    //                             <img src={user.photo} alt="profile photo of user"/>
+    //                             <button>View Profile</button>
+    //                         </div>
+    //                     )
+    //                 })
+    //             }
+    //         </section>
+    //         <footer></footer>
+    //     </main>
+    // )
 }
 
 
